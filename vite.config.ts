4c318608670f56c5fa1056c806fc0b5be029e848
mgmoh <mgm6597@gmail.com>
2025-04-21 03:23:16 -0400

    Additional change

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
-import path, { dirname } from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import { fileURLToPath } from "url";
+import { resolve } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    themePlugin(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
+// https://vitejs.dev/config/
+export default defineConfig(async () => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./client/src"),
+        "@shared": resolve(__dirname, "./shared"),
+      },
     },
-  },
-  root: path.resolve(__dirname, "client"),
-  build: {
-    outDir: path.resolve(__dirname, "dist", "public"),
-    emptyOutDir: true,
-  },
+    server: {
+      port: 3000,
+    },
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
+    },
+  };
 });
